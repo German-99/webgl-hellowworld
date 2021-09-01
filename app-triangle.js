b69d9fa6604c9f4539b1b15aeec920547ecbdefd
Adrian <adrian2009@live.com.mx>
2021-08-31 18:00:29 -0700

    Cuadrado  moviéndose

--- app-triangle.js
+++ app-triangle.js
@@ -2,38 +2,35 @@
 const canvas = document.getElementById('gl-canvas');
 const gl = canvas.getContext('webgl2');
 
-
-
-//Shader para possiciones
+//Shader para posiciones
 const vertexShader = `#version 300 es
 precision mediump float;
-
 in vec2 position;
 in vec3 iColor;
 out vec3 oColor;
 uniform float iTime;
 uniform mat4 modelMatrix;
-
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
 void main()
 {
-  gl_Position = modelMatrix * vec4(position, 0, 1);
-  oColor = vec3(abs(sin(iTime)), iColor.gb);
+  gl_Position = projectionMatrix * viewMatrix * (modelMatrix * vec4(position, 0, 1));
+  oColor = iColor;
 }
 `;
 
 //shader para color
 const fragmentShader = `#version 300 es
 precision mediump float;
-
 out vec4 fragColor;
 in vec3 oColor;
-
 void main()
 {
   fragColor = vec4(oColor, 1);
 }
 `;
 
+//Compiling shaders
 const vs = gl.createShader(gl.VERTEX_SHADER);
 const fs = gl.createShader(gl.FRAGMENT_SHADER);
 
@@ -44,11 +41,11 @@ gl.compileShader(vs);
 gl.compileShader(fs);
 
 if(!gl.getShaderParameter(vs, gl.COMPILE_STATUS)){
-    console.error(gl.getShaderInfoLog(vs));
+  console.error(gl.getShaderInfoLog(vs));
 }
 
 if(!gl.getShaderParameter(fs, gl.COMPILE_STATUS)){
-    console.error(gl.getShaderInfoLog(fs));
+  console.error(gl.getShaderInfoLog(fs));
 }
 
 const program = gl.createProgram();
@@ -57,85 +54,126 @@ gl.attachShader(program, fs);
 gl.linkProgram(program);
 
 if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
-    console.error(gl.getProgramInfoLog(program));
+  console.error(gl.getProgramInfoLog(program));
 }
 
 gl.useProgram(program);
 
 //Drawing basic triangle
 const triangleCoords = [
-    -0.2, 0.2,
-    -0.2, -0.2,
-    0.2, -0.2,
-
-    -0.2, 0.2,
-    0.2, -0.2,
-    0.2 , 0.2
-
+  -1, 1,
+  -1, -1,
+  1, -1,
+  1, 1
+  //-0.2, 0.2,
+  //0.2, -0.2,
+  //0.2, 0.2
 ];
 
 const vertexColorArray = [
   1, 0, 0, //r
   0, 1, 0, //g
   0, 0, 1, //b
-  1, 1, 0,
-  0, 1, 1,
-  0.5, 0.5, 1
+  1, 1, 1,
+  //0, 1, 1,
+  //0.5, 0.5, 1
 ];
 
-/*const indexArray = [
-0, 1, 2,
-2, 1, 3
-];*/
+const indexArray = [
+  0, 1, 2,
+  0, 2, 3
+];
 
- //const indexArrayBuffer = gl.createBuffer();
+const indexArrayBuffer = gl.createBuffer();
 const vertexColorBuffer = gl.createBuffer();
-const positionBuffer = gl.createBuffer();
+const postionBuffer = gl.createBuffer();
 
-const now = Date.now();
+let now = Date.now();
 
 const uniformTime = gl.getUniformLocation(program, 'iTime');
 const uModelMatrix = gl.getUniformLocation(program, 'modelMatrix');
+const uViewMatrix = gl.getUniformLocation(program, 'viewMatrix');
+const uProjectionMatrix = gl.getUniformLocation(program, 'projectionMatrix');
+const attribVertexColor = gl.getAttribLocation(program, 'iColor');
+const attribPosition = gl.getAttribLocation(program,'position');
 
 const modelMatrix = mat4.create();
+const viewMatrix = mat4.create();
+const projectionMatrix = mat4.create();
 
-const update = ()=>{
-  //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexArrayBuffer);
+mat4.scale(
+  viewMatrix,
+  viewMatrix,
+  [1, 1, 1]
+);
 
-  const deltaTime = Date.now() - now;
+mat4.translate(
+  viewMatrix,
+  viewMatrix,
+  [0, 0, -10]
+);
+
+const update = ()=> {
+
+  const deltaTime = (Date.now() - now) / 1000;
+  now = Date.now();
   //console.log(deltaTime);
 
   mat4.rotate(
     modelMatrix,
     modelMatrix,
-    deltaTime * 5.0,
-    [0, 0, 1]
+    deltaTime * 1,
+    [1, 1, 0]
   );
 
   //clear screen
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
+  /*mat4.translate(
+    modelMatrix,
+    modelMatrix,
+    [0, 0, 0]
+  );*/
 
-  gl.uniform1f(uniformTime, deltaTime / 1000);
+  /*mat4.translate(
+    viewMatrix,
+    viewMatrix,
+    [0, 0, -10]
+  );*/
+
+  mat4.perspective(
+    projectionMatrix,
+    45 * (Math.PI / 180),
+    canvas.clientWidth / canvas.clientHeight,
+    1,
+    1000
+  );
+
+  gl.uniform1f(uniformTime, deltaTime);
 
   gl.uniformMatrix4fv(uModelMatrix, false, modelMatrix);
-  
+  gl.uniformMatrix4fv(uViewMatrix, false, viewMatrix);
+  gl.uniformMatrix4fv(uProjectionMatrix, false, projectionMatrix);
+
+  //color
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColorArray), gl.STATIC_DRAW);
-  const attribVertexColor = gl.getAttribLocation(program, 'iColor');
   gl.enableVertexAttribArray(attribVertexColor);
   gl.vertexAttribPointer(attribVertexColor, 3, gl.FLOAT, gl.FALSE, 0, 0);
 
-  //Reservamos memoria en la tarjeta de video (vram)
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  //vertex
+  gl.bindBuffer(gl.ARRAY_BUFFER, postionBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleCoords), gl.STATIC_DRAW);
-
-  const attribPosition = gl.getAttribLocation(program,'position');
   gl.enableVertexAttribArray(attribPosition);
   gl.vertexAttribPointer(attribPosition, 2, gl.FLOAT, gl.FALSE, 0, 0);
-  //gl.drawElements(gl.GL_TRIANGLE, 6, GL.UNSIGNED_SHORT, 0)
-  gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+  //indices
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexArrayBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexArray), gl.STATIC_DRAW);
+
+  gl.drawElements(gl.TRIANGLES, indexArray.length, gl.UNSIGNED_SHORT, 0);
+  //gl.drawArrays(gl.TRIANGLES, 0, 6);
   requestAnimationFrame(update);
 }
 
